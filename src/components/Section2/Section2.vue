a77bce14609c33e7e4b024d004a0056f918e6fd4
Dr1n1 <64831432+Dr1n1@users.noreply.github.com>
2020-11-12 10:42:44 +0100

    Final push without content

--- src/components/Section2/Section2.vue
+++ src/components/Section2/Section2.vue
@@ -1,40 +1,103 @@
 <template>
   <div id="about">
-    <v-container class="main">
-      <h1 data-aos="fade-top" class="">Rreth nesh!</h1>
-      <v-row
-        class="container mt-sm-12"
-        justify-space-around
-        data-aos="fade-left"
-      >
-        <v-flex xs12 sm12 md6>
-          <img src="../../assets/work.jpg" alt class="img-fluid" />
-        </v-flex>
-        <v-flex xs12 sm12 md6 class="mt-sm-16" data-aos="fade-right">
-          <div class="ml-md-16">
-            <h2>Your Business On the Web</h2>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Veritatis, molestiae totam? Quibusdam vel qui, corporis nesciunt
-              esse sit sint. Consequatur minus dolore nihil ab? Veritatis libero
-              ipsam officia assumenda perferendis, eligendi ex cupiditate dolore
-              magni?
-            </p>
-          </div>
-        </v-flex>
+
+    <v-app id="inspire">
+      <h1 data-aos="fade-top" class="absolute">Rreth nesh!</h1>
+
+      <v-row align="center">
+        <v-item-group
+          v-model="window"
+          class="shrink mr-6"
+          mandatory
+          tag="v-flex"
+        >
+          <v-item
+            v-for="item in items"
+            :key="item.title"
+            v-slot="{ active, toggle }"
+          >
+            <div>
+              <v-btn :input-value="active" icon @click="toggle">
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-btn>
+            </div>
+          </v-item>
+        </v-item-group>
+
+        <v-col>
+          <v-window v-model="window" class="elevation-0" vertical>
+            <v-window-item v-for="item in items" :key="item.title">
+              <v-card flat>
+                <v-card-text>
+                  <v-row class="mb-4" align="center">
+                    <v-icon data-aos="fade-right">{{ item.icon }}</v-icon>
+                    <strong class="title" data-aos="fade-right">{{ item.title }}</strong>
+                    <v-spacer></v-spacer>
+                  </v-row>
+
+                  <v-row
+                    class="container mt-sm-12"
+                    justify-space-around
+                    data-aos="fade-left"
+                  >
+                    <v-flex xs12 sm12 md6>
+                      <img :src="item.image" alt class="img-fluid" />
+                    </v-flex>
+                    <v-flex
+                      xs12
+                      sm12
+                      md6
+                      class="mt-sm-16"
+                      data-aos="fade-right"
+                    >
+                      <div class="ml-md-16">
+                        <h2>{{ item.title }}</h2>
+                        <p>
+                          {{ item.description }}
+                        </p>
+                      </div>
+                    </v-flex>
+                  </v-row>
+                </v-card-text>
+              </v-card>
+            </v-window-item>
+          </v-window>
+        </v-col>
       </v-row>
-    </v-container>
+    </v-app>
   </div>
 </template>
 
-<style scoped>
-.main {
-  margin-top: 100px;
-}
+<script>
+export default {
+  data: () => ({
+    window: 0,
+    items: [
+      {
+        title: " Flash",
+        icon: "mdi-flash",
+        description:"LoremLorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis, molestiae totam? Quibusdam vel qui, corporis nesciunt esse sit sint. Consequatur minus dolore nihil ab? Veritatis libero ipsam officia assumenda perferendis, eligendi ex cupiditate dolore magni?",
+        image: require("../../assets/work.jpg"),
+      },
+      {
+        title: " Bookmark",
+        icon: " mdi-bookmark-multiple-outline",
+        description:"LoremLorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis, molestiae totam? Quibusdam vel qui, corporis nesciunt esse sit sint. Consequatur minus dolore nihil ab? Veritatis libero ipsam officia assumenda perferendis, eligendi ex cupiditate dolore magni?",
+        image: require("../../assets/work.jpg"),
+      },
+      {
+        title: " Book",
+        icon: "mdi-book-open-variant",
+        description:"LoremLorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis, molestiae totam? Quibusdam vel qui, corporis nesciunt esse sit sint. Consequatur minus dolore nihil ab? Veritatis libero ipsam officia assumenda perferendis, eligendi ex cupiditate dolore magni?",
+        image: require("../../assets/work.jpg"),
+      },
+    ],
+  }),
+};
+</script>
 
-@media only screen and (max-width: 960px) {
-  .description {
-    margin-top: 70px;
-  }
+<style lang="scss" scoped>
+.v-application .mr-6 {
+  margin-left: 50px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
 <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- IMPORT COMPONENTS HERE IN ORDER -->
+
     <LandingView />
     <Section1 />
-    <Section2 />
     <Section3 />
+    <Section2 />
     <ContactForm />
-    <Section5 />
 </div>
 </template>
 
@@ -18,7 +16,6 @@ import Section1 from "@/components/Section1/Section1.vue";
 import Section2 from "@/components/Section2/Section2.vue";
 import Section3 from "@/components/Section3/Section3.vue";
 import ContactForm from "@/components/Section4/ContactForm.vue";
-import Section5 from "@/components/Section5/Section5.vue";
 
 export default {
     name: "Home",
@@ -28,7 +25,6 @@ export default {
         Section2,
         Section3,
         ContactForm,
-        Section5,
     },
 };
 </script>
